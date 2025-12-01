1c1b6f0f5a4d5867712887a407fc428ed1690ecf
채민희 <chaeminhui@chaeminhuiui-MacBookAir.local>
2025-12-01 16:47:16 +0900

    profile, search area 추가

--- src/components/top/TopNav.vue
+++ src/components/top/TopNav.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="flex items-top justify-between w-full mx-auto px-4 sm:px-6 lg:px-8">
+  <div class="flex items-top justify-between w-full mx-auto px-4 sm:px-6 lg:px-8 pt-2">
     <div class="flex flex-1">
-      <TopNavMemu :menus="menus" class="hidden space-x-1 md:-my-px md:ml-10 md:flex"/>
+      <TopNavMemu :menus="menus" class="hidden space-x-1 md:ml-10 md:flex items-center"/>
+      <div class="flex flex-1 items-center pl-2">
+        <SearchInput class="ml-2 mr-4 w-full max-w-md"></SearchInput>
+      </div>
+       <div class="hidden md:flex md:items-center">
+        profile area
+       </div>
     </div>
   </div>
 </template>
 <script>
 import { defineComponent, onMounted} from 'vue';
 import TopNavMemu from './TopNavMemu.vue';
+import SearchInput from './search/SearchInput.vue';
 export default defineComponent({
   components: {
     TopNavMemu,
+    SearchInput
   },
   props: {
     menus: {
---
+++ src/components/top/search/SearchInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class ="relative ">
+    <div class="relative flex items-center border h-12 border-theme-primary bg-theme-bg-search-default rounded-md focus:outline-none focus:border-transparent focus:ring-0">
+      <SearchIcon
+          class="absolute ml-2 h-6 w-6 text-theme-primary cursor-pointer"
+        />
+      <input class="h-10 w-full pl-10 pr-10 outline-none text-theme-text-defualt rounded-md border-0 bg-transparent focus:ring-0"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue';
+import { SearchIcon} from '@heroicons/vue/outline';
+export default defineComponent({
+  components: {
+    SearchIcon,
+  },
+  setup() {
+    const state = reactive({
+    })
+
+
+    return {
+      ...toRefs(state),
+    };
+  },
+});
+</script>
+<style scoped>
+</style>
